3a93c2b45ae1223a85bf74748e7eb7641e75add7
Ryan-Chill <53105786+Ryan-Chill@users.noreply.github.com>
2022-06-19 19:56:26 +0800

    Update main.py

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ client = discord.Client()
 
 def fetch_role(m):
   author_roles = [r.id for r in m.author.roles]
-  for i, n in enumerate([985750157607985153, 987232675221868604, 987232721673814016]):
+  for i, n in enumerate([985750157607985153, 987232675221868604]):
     if n in author_roles:
       return i
   return None
@@ -20,7 +20,7 @@ async def on_ready():
   print(f'{client.user} has connected to Discord!')
 @client.event
 async def on_message(message):
-  if message.channel.id == 985752337710723163:
+  if message.channel.id == 985752337710723163 and not message.content.startswith("$"):
     counting_team = fetch_role(message)
     if counting_team is None:
       return None
@@ -32,31 +32,28 @@ async def on_message(message):
         
     f = open('number.txt', 'r')
     current_count = int(f.readline())
-    correct_lst = f.readline().split(",")
+    correct_lst = f.readline()[:-1].split(",")
     error_lst = f.readline().split(",")
     f.close()
         
     if not wrong_counting and entry_count == current_count + 1:
       current_count += 1
       correct_lst[counting_team] = str(int(correct_lst[counting_team]) + 1)
-      f = open('number.txt', 'w')
-      f.writelines([str(current_count) + "\n", ",".join(correct_lst), ",".join(error_lst)])
-      f.close()
     else:
       error_lst[counting_team] = str(int(error_lst[counting_team]) + 1)
-      f = open('number.txt', 'w')
-      f.writelines([str(current_count) + "\n", ",".join(correct_lst), ",".join(error_lst)])
-      f.close()
+    
+    f = open('number.txt', 'w')
+    f.writelines([str(current_count) + "\n", ",".join(correct_lst) + "\n", ",".join(error_lst)])
+    f.close()
       
   if message.content.startswith("$score"):
     f = open('number.txt', 'r')
     f.readline()
-    correct_lst = f.readline().split(",")
+    correct_lst = f.readline()[:-1].split(",")
     error_lst = f.readline().split(",")
     f.close()
-    correct_lst[2] = correct_lst[2][:-1]
     output = "This command is used for debugging and testing, not to be actually implemented"
-    for i, name in enumerate(["Team Kusanali", "Team Itto", "Team Kuki"]):
+    for i, name in enumerate(["Team Kusanali", "Team Yaoyao"]):
       output += f"\n{name}: has {correct_lst[i]} correct entries and {error_lst[i]} wrong entries"
     await message.channel.send(output)
 
